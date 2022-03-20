0e0b226061fe2c431e2f5319020447dc92e83909
changsup <changsup96@gmail.com>
2022-03-20 17:29:16 +0900

    Remove @SpringBooTest annotation.

--- src/test/java/com/csup96/appt_notifier/ApptNotifierApplicationTests.java
+++ src/test/java/com/csup96/appt_notifier/ApptNotifierApplicationTests.java
@@ -3,7 +3,7 @@ package com.csup96.appt_notifier;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest: AWS test 오류 발생
 class ApptNotifierApplicationTests {
 
 	@Test
